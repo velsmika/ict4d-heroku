8ff3ea9166c56d8ab82b93d6e786cdb78fc1bc5e
Mika Vels <mika.vels@hva.nl>
2024-04-25 19:17:12 +0200

    pls work

--- ict4d/forms.py
+++ ict4d/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 
 class UploadFileForm(forms.Form):
+    audio_type = forms.CharField(max_length=10)
     audio_name= forms.CharField(max_length=50)
+    lang = forms.CharField(max_length=2)
     file = forms.FileField()
